80844e5c2ab09060e9c28c83cb36c2b61887460e
Grant Leadbetter <grant@nespite.io>
2015-06-30 15:54:29 +0100

    added get method to return all users at /api/users

--- server.js
+++ server.js
@@ -52,6 +52,14 @@ apiRouter.route('/users')
 				}
 					res.json({message: 'User created!'});
 			});
+	})
+
+	.get(function(req, res){
+		User.find(function(err, users){
+			if (err) res.send(err);
+
+			res.json(users);
+		});
 	});
 
 app.use('/api', apiRouter);
