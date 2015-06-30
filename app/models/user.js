a6e46929b458902239c0469c90a9508d280ef080
Grant Leadbetter <grant@nespite.io>
2015-06-30 15:38:17 +0100

    fixed typo in schema

--- app/models/user.js
+++ app/models/user.js
@@ -2,7 +2,7 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 var bcrypt = require('bcrypt-nodejs');
 
-var UserScema = new Schema({
+var UserSchema = new Schema({
 	name: String,
 	username: { type: String, required: true, index: { unique: true }},
 	password: { type: String, required: true, select: false }
